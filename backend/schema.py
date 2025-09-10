ab42c4af0e8aa37b1beeb2e7d2dbaf4a554b4a5f
Lindon Morais <lindon.jonhsonf@gmail.com>
2025-09-10 18:27:56 -0300

    added the schema

--- backend/schema.py
+++ backend/schema.py
@@ -10,4 +10,19 @@ class ProductBase(BaseModel):
     categoria: str
     email_fornecedor: EmailStr
 
-class ProductCreate(ProductBase)
+class ProductCreate(ProductBase): # create
+    pass
+
+class ProductResponse(ProductBase): # read
+    id: int
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+class ProductUpdate(BaseModel): # update
+    name: Optional[str] = None
+    description: Optional[str] = None
+    price: Optional[PositiveFloat] = None
+    categoria: Optional[str] = None
+    email_fornecedor: Optional[EmailStr] = None
